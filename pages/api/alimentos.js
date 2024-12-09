162316425c758c0f3cbdf1ba178f2a9864090e47
Henrique0440 <59101905+Henrique0440@users.noreply.github.com>
2024-12-09 19:40:11 -0300

    Add files via upload

---
+++ pages/api/alimentos.js
@@ -0,0 +1,27 @@
+
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+export default async function handler(req, res) {
+    const MONGO_URI = process.env.MONGO_URI;
+
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+    try {
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        const db = await client.db("cardapio_rodinho")
+        const collection = await db.collection("cardapio")
+
+        const documentos = await collection.find({}).toArray();
+        res.status(200).json(documentos);
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao conectar ao banco de dados', error: error.message });
+    } finally {
+        await client.close();
+    }
+}
