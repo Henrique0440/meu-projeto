4d2f948756a191d3dd95075fafbdea73931d97d7
Henrique0440 <59101905+Henrique0440@users.noreply.github.com>
2024-12-10 11:38:11 -0300

    Update alimentos.js

--- pages/api/alimentos.js
+++ pages/api/alimentos.js
@@ -5,6 +5,9 @@ dotenv.config(); // Carrega as variáveis de ambiente
 
 export default async function handler(req, res) {
     const MONGO_URI = process.env.MONGO_URI;
+    if (!MONGO_URI) {
+    return res.status(500).json({ message: "A variável de ambiente MONGO_URI não está definida." });
+}
     if (req.method !== 'GET') {
         res.status(405).json({ message: 'Método não permitido' });
         return;
