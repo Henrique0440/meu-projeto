ba78598cb55dd7abc9b67de7f4d982b48b5cc4e5
Henrique0440 <59101905+Henrique0440@users.noreply.github.com>
2024-12-12 17:08:31 -0300

    Update alimentos.js

--- pages/api/alimentos.js
+++ pages/api/alimentos.js
@@ -13,14 +13,10 @@ export default async function handler(req, res) {
     if (req.method === 'OPTIONS') {
         return res.status(204).end();
     }
-    
+
     const MONGO_URI = process.env.MONGO_URI;
     if (!MONGO_URI) {
-    return res.status(500).json({ message: "A variável de ambiente MONGO_URI não está definida." });
-}
-    if (req.method !== 'GET') {
-        res.status(405).json({ message: 'Método não permitido' });
-        return;
+        return res.status(500).json({ message: "A variável de ambiente MONGO_URI não está definida." });
     }
 
     const client = new MongoClient(MONGO_URI, {
@@ -37,8 +33,28 @@ export default async function handler(req, res) {
         console.log("Conexão com o MongoDB estabelecida.");
         const db = await client.db("cardapio_rodinho");
         const collection = await db.collection("cardapio");
-        const documentos = await collection.find({}).toArray();
-        res.status(200).json(documentos);
+        
+        if (req.method === 'GET') {
+            const documentos = await collection.find({}).toArray();
+            res.status(200).json(documentos);
+        }
+        else if (req.method === 'POST') {
+            const { name, description, price, link } = req.body;
+
+            if (!name || !description || !price || !link) {
+                return res.status(400).json({ error: "Todos os campos são obrigatórios." })
+            }
+
+            const novoItem = { name, description, price, link };
+            const result = await collection.insertOne(novoItem);
+
+            return res.status(201).json({
+                message: "Item adicionado ao cardápio com sucsso!",
+                id: result.insertedId,
+            })
+        } else {
+            res.status(405).json({ message: "Método não permitido" })
+        }
     } catch (error) {
         res.status(500).json({ message: 'Erro ao conectar ao banco de dados', error: error.message });
     } finally {
